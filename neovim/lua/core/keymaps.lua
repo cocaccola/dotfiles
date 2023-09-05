2a73171206dfdd2c54f61177507bd3c15844a2b3
Cameron Accola <37202596+cocaccola@users.noreply.github.com>
2023-09-04 20:35:00 -0500

    backup wip commit

--- neovim/lua/core/keymaps.lua
+++ neovim/lua/core/keymaps.lua
@@ -15,6 +15,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- move lines of code around
+-- see https://vim.fandom.com/wiki/Moving_lines_up_or_down#Mappings_to_move_lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
