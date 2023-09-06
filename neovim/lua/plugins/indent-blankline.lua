aaa84d7c6f100a4c1bab1e902cc292ea858d25ad
Cameron Accola <37202596+cocaccola@users.noreply.github.com>
2023-09-06 17:38:06 -0500

    backup wip commit

---
+++ neovim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,12 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+        require("indent_blankline").setup {
+            char = "▎",
+            context_char = "▎",
+            space_char_blankline = " ",
+            show_current_context = true,
+            show_current_context_start = true,
+        }
+    end,
+}
