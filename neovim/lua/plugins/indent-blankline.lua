9a0516c7750663aa74177a9ccccf902a0c901035
Cameron Accola <37202596+cocaccola@users.noreply.github.com>
2023-09-06 17:54:58 -0500

    backup wip commit

--- neovim/lua/plugins/indent-blankline.lua
+++ neovim/lua/plugins/indent-blankline.lua
@@ -7,6 +7,7 @@ return {
             space_char_blankline = " ",
             show_current_context = true,
             show_current_context_start = true,
+            use_treesitter_scope = true,
         }
     end,
 }
