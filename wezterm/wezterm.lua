994fa401436fb8f7ff50c36a6bfa8ef51f5b8da4
Cameron Accola <37202596+cocaccola@users.noreply.github.com>
2023-11-08 00:17:25 -0600

    updates

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,18 +9,35 @@ end
 config.front_end = 'WebGpu'
 config.webgpu_power_preference = 'HighPerformance'
 config.color_scheme = 'Catppuccin Macchiato'
+
+local font_name = 'FiraCode Nerd Font Mono'
+config.font_size = 14.0
+
+config.font = wezterm.font(
+    font_name,
+    { weight = 'Thin' }
+)
+config.font_rules = {
+    {
+        intensity = 'Bold',
+        font = wezterm.font(
+            font_name,
+            { weight = 'Bold' }
+        )
+    }
+}
+
+config.freetype_load_flags = 'NO_HINTING'
+
 -- disable ligatures
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
-config.font = wezterm.font('FiraCode Nerd Font Mono')
-config.font_size = 14.0
-config.freetype_load_target = 'Light'
 
+config.audible_bell = 'Disabled'
 config.hide_tab_bar_if_only_one_tab = true
 config.scrollback_lines = 100000
 config.default_cursor_style = 'BlinkingBlock'
 config.cursor_blink_rate = 700
 config.cursor_blink_ease_in = 'Constant'
 config.cursor_blink_ease_out = 'Constant'
-
 
 return config
