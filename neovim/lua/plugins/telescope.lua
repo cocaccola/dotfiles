36c07a2b1c33c83e7578262b1a244e1533fe3d56
Cameron Accola <37202596+cocaccola@users.noreply.github.com>
2024-01-22 14:51:39 -0600

    updates

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -61,7 +61,7 @@ return {
 
         vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]how [D]iagnostics' })
         vim.keymap.set('n', '<leader>ss', builtin.lsp_document_symbols, { desc = '[S]earch [S]ymbols' })
-        vim.keymap.set('n', '<leader>ws', builtin.lsp_workspace_symbols, { desc = '[W]orkspace [S]ymbols' })
+        vim.keymap.set('n', '<leader>sw', builtin.lsp_workspace_symbols, { desc = '[S]ymbols in [W]orkspace' })
         vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = '[F]ind [R]eferences' })
         vim.keymap.set('n', '<leader>fi', builtin.lsp_implementations, { desc = '[F]ind [I]mplementations' })
         vim.keymap.set('n', '<leader>fd', builtin.lsp_definitions, { desc = '[F]ind [D]definitions' })
