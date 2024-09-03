97269478df4b3b0ba116882824e3ed904b874ec7
Cameron <37202596+cocaccola@users.noreply.github.com>
2024-09-03 14:13:12 -0500

    upgrade lsp-zero (#50)

--- neovim/lua/plugins/lsp.lua
+++ neovim/lua/plugins/lsp.lua
@@ -1,77 +1,109 @@
 return {
+  {
     'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    dependencies = {
-        -- LSP Support
-        { 'neovim/nvim-lspconfig' },             -- Required
-        { 'williamboman/mason.nvim' },           -- Optional
-        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+    branch = 'v4.x',
+    lazy = true,
+    config = false,
+  },
+  {
+    'williamboman/mason.nvim',
+    lazy = false,
+    config = true,
+  },
 
-        -- Autocompletion
-        { 'hrsh7th/nvim-cmp' },     -- Required
-        { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-        { 'L3MON4D3/LuaSnip' },     -- Required
+  -- Autocompletion
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      {'L3MON4D3/LuaSnip'},
     },
     config = function()
-        local lsp = require('lsp-zero')
+      local cmp = require('cmp')
 
-        lsp.on_attach(function(_, bufnr)
-            -- see :help lsp-zero-keybindings
-            -- to learn the available actions
-            lsp.default_keymaps({
-                buffer = bufnr,
-                preserve_mappings = false,
-            })
-            -- see https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/lsp.md#enable-format-on-save
-            lsp.buffer_autoformat()
+      cmp.setup({
+        sources = {
+          {name = 'nvim_lsp'},
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<Tab>'] = vim.NIL,
+          ['<S-Tab>'] = vim.NIL,
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        snippet = {
+          expand = function(args)
+            vim.snippet.expand(args.body)
+          end,
+        },
+      })
+    end
+  },
 
-            vim.keymap.set('n', '<leader>st', '<cmd>lua vim.lsp.buf.document_symbol()<cr>',
-                { buffer = true, desc = "[S]ymbol [T]able of Contents" })
-            vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>',
-                { buffer = true, desc = "[R]e [N]ame using lsp" })
-            vim.keymap.set({ 'n', 'x' }, '<leader>fm', '<cmd>lua vim.lsp.buf.format({async = true})<cr>',
-                { buffer = true, desc = "[F]or[M]at using lsp" })
-            vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>',
-                { buffer = true, desc = "[C]ode [A]ction" })
-        end)
+  -- LSP
+  {
+    'neovim/nvim-lspconfig',
+    cmd = {'LspInfo', 'LspInstall', 'LspStart'},
+    event = {'BufReadPre', 'BufNewFile'},
+    dependencies = {
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+    },
+    config = function()
+      local lsp_zero = require('lsp-zero')
 
-        -- (Optional) Configure lua language server for neovim
-        require('mason').setup({})
-        require('mason-lspconfig').setup({
-            ensure_installed = { 'tsserver', 'rust_analyzer' },
-            handlers = {
-                lsp.default_setup,
-                lua_ls = function()
-                    local lua_opts = lsp.nvim_lua_ls()
-                    require('lspconfig').lua_ls.setup(lua_opts)
-                end,
-            }
+      -- lsp_attach is where you enable features that only work
+      -- if there is a language server active in the file
+      local lsp_attach = function(client, bufnr)
+        local opts = {buffer = bufnr}
+
+        lsp_zero.default_keymaps({
+            buffer = bufnr,
+            preserve_mappings = false,
         })
 
+        lsp_zero.buffer_autoformat()
 
-        lsp.setup()
+        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 
-        local cmp = require('cmp')
-        local cmp_format = require('lsp-zero').cmp_format()
-        -- local cmp_action = require('lsp-zero').cmp_action()
-        cmp.setup({
-            formatting = cmp_format,
-            mapping = cmp.mapping.preset.insert({
-                -- attempt to disable tab for cmp
-                ['<Tab>'] = vim.NIL,
-                ['<S-Tab>'] = vim.NIL,
-                ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                -- super tab, this might be bugged
-                -- ['<Tab>'] = cmp_action.luasnip_supertab(),
-                -- ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-                -- try regular tab completion
-                -- ['<Tab>'] = cmp_action.tab_complete(),
-                -- ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-                -- manually open menu
-                -- ['<C-Space>'] = cmp.mapping.complete()
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
-            })
-        })
-    end,
+        vim.keymap.set('n', '<leader>st', '<cmd>lua vim.lsp.buf.document_symbol()<cr>',
+            { buffer = true, desc = "[S]ymbol [T]able of Contents" })
+        vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>',
+            { buffer = true, desc = "[R]e [N]ame using lsp" })
+        vim.keymap.set({ 'n', 'x' }, '<leader>fm', '<cmd>lua vim.lsp.buf.format({async = true})<cr>',
+            { buffer = true, desc = "[F]or[M]at using lsp" })
+        vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>',
+            { buffer = true, desc = "[C]ode [A]ction" })
+      end
+
+      lsp_zero.extend_lspconfig({
+        sign_text = true,
+        lsp_attach = lsp_attach,
+        capabilities = require('cmp_nvim_lsp').default_capabilities()
+      })
+
+      require('mason-lspconfig').setup({
+        ensure_installed = {},
+        handlers = {
+          -- this first function is the "default handler"
+          -- it applies to every language server without a "custom handler"
+          function(server_name)
+            require('lspconfig')[server_name].setup({})
+          end,
+        }
+      })
+    end
+  }
 }
+
