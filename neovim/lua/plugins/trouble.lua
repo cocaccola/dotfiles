23318154c632fca8188c101f713d0e2dc3b36922
Cameron Accola <37202596+cocaccola@users.noreply.github.com>
2024-09-18 20:16:34 -0500

    update neovim config

--- neovim/lua/plugins/trouble.lua
+++ neovim/lua/plugins/trouble.lua
@@ -1,26 +1,54 @@
 return {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = "Trouble",
+    keys = {
+        {
+            "<leader>xx",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>xb",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+            "<leader>cs",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Symbols (Trouble)",
+        },
+        {
+            "<leader>cl",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+            "<leader>xl",
+            "<cmd>Trouble loclist toggle<cr>",
+            desc = "Location List (Trouble)",
+        },
+        {
+            "<leader>qf",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Quickfix List (Trouble)",
+        },
+    },
     config = function()
-        require('trouble').setup()
-        vim.keymap.set("n", "<leader>xo", function() require("trouble").open() end, { desc = "Open Diagnostics" })
-        vim.keymap.set("n", "<leader>xp", function() require("trouble").open("workspace_diagnostics") end,
-            { desc = "Workspace (project) Diagnostics" })
-        vim.keymap.set("n", "<leader>xx", function() require("trouble").open("document_diagnostics") end,
-            { desc = "Document Diagnostics" })
-        vim.keymap.set("n", "<leader>qf", function() require("trouble").open("quickfix") end, { desc = "Quick Fix" })
-        vim.keymap.set("n", "<leader>xl", function() require("trouble").open("loclist") end, { desc = "loclist" })
-        vim.keymap.set("n", "gR", function() require("trouble").open("lsp_references") end, { desc = "lsp references" })
-        vim.keymap.set("n", "<leader>xc", function() require("trouble").close(opts) end, { desc = "close Trouble" })
-
+        local opts = {}
+        require("trouble").setup(opts)
         vim.keymap.set("n", "<M-j>", function()
-            require("trouble").next(opts)
-            require("trouble").jump(opts)
+            require("trouble").next({})
+            require("trouble").jump({})
         end, { desc = "Trouble next item" })
 
         vim.keymap.set("n", "<M-k>", function()
-            require("trouble").prev(opts)
-            require("trouble").jump(opts)
+            require("trouble").prev({})
+            require("trouble").jump({})
         end, { desc = "Trouble prev item" })
+
+        vim.keymap.set("n", "<leader>xc", function()
+            require("trouble").close({})
+        end, { desc = "close Trouble" })
     end,
 }
