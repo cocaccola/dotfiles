d75b6e7273054c462e20e9cbbbc8a3b233b8f3a1
Cameron Accola <37202596+cocaccola@users.noreply.github.com>
2024-09-30 18:24:19 -0500

    add yaml ftplugin config

---
+++ neovim/after/ftplugin/yaml.lua
@@ -0,0 +1,4 @@
+vim.opt_local.expandtab = true    -- Use spaces instead of tabs
+vim.opt_local.shiftwidth = 2      -- Number of spaces to use for auto-indentation
+vim.opt_local.tabstop = 2         -- Number of spaces that a <Tab> in the file counts for
+vim.opt_local.softtabstop = 2     -- Number of spaces that a <Tab> in insert mode counts for
