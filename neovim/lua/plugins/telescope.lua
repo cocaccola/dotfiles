56c571f0909a2f23d38aeccb1dcbcddb8d6e578c
Cameron Accola <37202596+cocaccola@users.noreply.github.com>
2024-12-13 00:53:59 -0600

    stuff

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -39,6 +39,7 @@ return {
                 },
             },
             extensions = {
+                fzf = {},
                 ['ui-select'] = {
                     require('telescope.themes').get_dropdown(),
                 },
