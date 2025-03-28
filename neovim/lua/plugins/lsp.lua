e1234886cea12af27df50613b090a0f0faf25f86
Cameron <37202596+cocaccola@users.noreply.github.com>
2025-03-27 19:56:23 -0500

    update neovim configs (#55)

--- neovim/lua/plugins/lsp.lua
+++ neovim/lua/plugins/lsp.lua
@@ -1,14 +1,19 @@
 return {
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v4.x',
-        lazy = true,
-        config = false,
-    },
-    {
         'williamboman/mason.nvim',
         lazy = false,
         config = true,
+    },
+
+    -- Snippets
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        version = "v2.*",
+        build = "make install_jsregexp",
+        config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end,
     },
 
     -- Autocompletion
@@ -17,19 +22,20 @@ return {
         event = 'InsertEnter',
         dependencies = {
             { 'L3MON4D3/LuaSnip' },
+            { 'saadparwaiz1/cmp_luasnip' },
             { 'hrsh7th/cmp-buffer' },
             { 'hrsh7th/cmp-path' },
-            { "rafamadriz/friendly-snippets" },
+            { "onsails/lspkind.nvim" },
         },
         config = function()
             local cmp = require('cmp')
-
-            local cmp_action = require('lsp-zero').cmp_action()
-            local cmp_format = require('lsp-zero').cmp_format({ details = true })
-
-            require('luasnip.loaders.from_vscode').lazy_load()
+            local lspkind = require('lspkind')
 
             cmp.setup({
+                preselect = cmp.PreselectMode.None,
+                completion = {
+                    completeopt = 'menu,menuone,noinsert',
+                },
                 sources = {
                     { name = 'nvim_lsp' },
                     { name = 'luasnip' },
@@ -42,15 +48,42 @@ return {
                     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-d>'] = cmp.mapping.scroll_docs(4),
                     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-                    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+                    -- Jump to the next snippet placeholder
+                    ['<C-f>'] = cmp.mapping(function(fallback)
+                        local luasnip = require('luasnip')
+                        if luasnip.locally_jumpable(1) then
+                            luasnip.jump(1)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                    -- Jump to the previous snippet placeholder
+                    ['<C-b>'] = cmp.mapping(function(fallback)
+                        local luasnip = require('luasnip')
+                        if luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
                 }),
                 snippet = {
                     expand = function(args)
                         require('luasnip').lsp_expand(args.body)
                     end,
                 },
-                formatting = cmp_format,
+                formatting = {
+                    format = lspkind.cmp_format({
+                        mode = 'symbol_text',
+                        show_labelDetails = true,
+
+                        -- The function below will be called before any actual modifications from lspkind
+                        -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+                        before = function(entry, vim_item)
+                            return vim_item
+                        end
+                    })
+                },
             })
         end
     },
@@ -66,64 +99,62 @@ return {
             { 'williamboman/mason-lspconfig.nvim' },
         },
         config = function()
-            local lsp_zero = require('lsp-zero')
+            vim.opt.signcolumn = 'yes'
+
+            -- Add cmp_nvim_lsp capabilities settings to lspconfig
+            -- This should be executed before you configure any language server
+            local lspconfig_defaults = require('lspconfig').util.default_config
+            lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+                'force',
+                lspconfig_defaults.capabilities,
+                require('cmp_nvim_lsp').default_capabilities()
+            )
 
             -- lsp_attach is where you enable features that only work
             -- if there is a language server active in the file
-            local lsp_attach = function(client, bufnr)
-                local opts = { buffer = bufnr }
+            vim.api.nvim_create_autocmd('LspAttach', {
+                desc = 'LSP actions',
+                callback = function(event)
+                    local opts = { buffer = event.buf }
 
-                lsp_zero.default_keymaps({
-                    buffer = bufnr,
-                    preserve_mappings = false,
-                })
+                    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 
-                lsp_zero.buffer_autoformat()
+                    vim.keymap.set('n', '<leader>st', '<cmd>lua vim.lsp.buf.document_symbol()<cr>',
+                        { buffer = true, desc = "[S]ymbol [T]able of Contents" })
+                    vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>',
+                        { buffer = true, desc = "[R]e [N]ame using lsp" })
+                    vim.keymap.set({ 'n', 'x' }, '<leader>fm', '<cmd>lua vim.lsp.buf.format({async = true})<cr>',
+                        { buffer = true, desc = "[F]or[M]at using lsp" })
+                    vim.keymap.set('n', '<leader>ca', function()
+                        vim.lsp.buf.code_action({
+                            filter = function(action)
+                                local unwanted_patterns = {
+                                    "Browse gopls feature documentation",
+                                    "Browse amd64 assembly for",
+                                    "Browse arm64 assembly for"
+                                }
 
-                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-                vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-                vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-                vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-
-                vim.keymap.set('n', '<leader>st', '<cmd>lua vim.lsp.buf.document_symbol()<cr>',
-                    { buffer = true, desc = "[S]ymbol [T]able of Contents" })
-                vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>',
-                    { buffer = true, desc = "[R]e [N]ame using lsp" })
-                vim.keymap.set({ 'n', 'x' }, '<leader>fm', '<cmd>lua vim.lsp.buf.format({async = true})<cr>',
-                    { buffer = true, desc = "[F]or[M]at using lsp" })
-                vim.keymap.set('n', '<leader>ca', function()
-                    vim.lsp.buf.code_action({
-                        filter = function(action)
-                            local unwanted_patterns = {
-                                "Browse gopls feature documentation",
-                                "Browse amd64 assembly for",
-                                "Browse arm64 assembly for",
-                                "Browse documentation for package"
-                            }
-
-                            for _, pattern in ipairs(unwanted_patterns) do
-                                if action.title:match(pattern) then
-                                    return false
+                                for _, pattern in ipairs(unwanted_patterns) do
+                                    if action.title:match(pattern) then
+                                        return false
+                                    end
                                 end
-                            end
 
-                            return true
-                        end,
-                        apply = false,
-                    })
-                end, { buffer = true, desc = "[C]ode [A]ction" })
-            end
-
-            lsp_zero.extend_lspconfig({
-                sign_text = true,
-                lsp_attach = lsp_attach,
-                capabilities = require('cmp_nvim_lsp').default_capabilities()
+                                return true
+                            end,
+                            apply = false,
+                        })
+                    end, { buffer = true, desc = "[C]ode [A]ction" })
+                end,
             })
 
             vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
