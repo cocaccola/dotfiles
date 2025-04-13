ff48bb4d1404076a07d20b4dfd8beddd73925d60
Cameron Accola <37202596+cocaccola@users.noreply.github.com>
2025-04-13 00:51:45 -0500

    add conform.nvim

---
+++ neovim/lua/plugins/conform.lua
@@ -0,0 +1,24 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    -- This will provide type hinting with LuaLS
+    ---@module "conform"
+    ---@type conform.setupOpts
+    opts = {
+        -- Define your formatters
+        formatters_by_ft = {
+            javascript = { "prettierd", "prettier", stop_after_first = true },
+        },
+        -- Set default options
+        default_format_opts = {
+            lsp_format = "fallback",
+        },
+        -- Set up format-on-save
+        format_on_save = { timeout_ms = 500 },
+    },
+    init = function()
+        -- If you want the formatexpr, here is the place to set it
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+}
