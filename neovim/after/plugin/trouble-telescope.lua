ae2cefa8c3ab0d926d3d63a903cb49f798c09e92
Cameron Accola <37202596+cocaccola@users.noreply.github.com>
2025-04-28 17:01:17 -0500

    fix trouble telescope

--- neovim/after/plugin/trouble-telescope.lua
+++ neovim/after/plugin/trouble-telescope.lua
@@ -1,18 +1,11 @@
-local actions = require("telescope.actions")
 local telescope = require("telescope")
-
-function open_qf_trouble(prompt_bufnr)
-    -- Send selected items to the quickfix list
-    actions.send_to_qflist(prompt_bufnr)
-    -- Open Trouble in quickfix mode
-    vim.cmd("Trouble qflist toggle")
-end
+local open_with_trouble = require("trouble.sources.telescope").open
 
 telescope.setup({
-  defaults = {
-    mappings = {
-      i = { ["<c-t>"] = open_qf_trouble },
-      n = { ["<c-t>"] = open_qf_trouble },
+    defaults = {
+        mappings = {
+            i = { ["<c-t>"] = open_with_trouble },
+            n = { ["<c-t>"] = open_with_trouble },
+        },
     },
-  },
 })
