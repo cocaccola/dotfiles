3affbbbc28be8a5e57a129e22d22b2afe2e628ce
Cameron Accola <37202596+cocaccola@users.noreply.github.com>
2025-05-07 11:31:40 -0500

    update git blame highlights

--- neovim/lua/plugins/catppuccin.lua
+++ neovim/lua/plugins/catppuccin.lua
@@ -8,6 +8,7 @@ return {
             transparent_background = vim.g.transparent_enabled,
             custom_highlights = {
                 LineNr = { fg = palette.rosewater },
+                GitSignsCurrentLineBlame = { fg = palette.surface2 },
             },
         })
         vim.cmd.colorscheme("catppuccin-mocha")
