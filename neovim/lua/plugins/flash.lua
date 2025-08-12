9b8f9c0468c600d6a13dfb8acb3c3b2845037d09
Cameron Accola <37202596+cocaccola@users.noreply.github.com>
2025-08-12 15:07:08 -0500

    fix flash

--- neovim/lua/plugins/flash.lua
+++ neovim/lua/plugins/flash.lua
@@ -8,7 +8,8 @@ return {
                 enabled = true
             },
             char = {
-                jump_labels = true
+                jump_labels = true,
+                label = { exclude = "hjkliardcsx" },
             }
         }
     },
--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -234,11 +234,12 @@ return {
 
             local ts_repeat_move = require "nvim-treesitter-textobjects.repeatable_move"
 
-            -- vim way: ; goes to the direction you were moving.
-            vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move, { expr = true })
-            vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite, { expr = true })
+            -- NOTE: this might be bugged: https://github.com/nvim-treesitter/nvim-treesitter-textobjects/issues/775
+            -- -- vim way: ; goes to the direction you were moving.
+            vim.keymap.set({ "n", "x", "o" }, "<leader>;", ts_repeat_move.repeat_last_move, { expr = true })
+            vim.keymap.set({ "n", "x", "o" }, "<leader>,", ts_repeat_move.repeat_last_move_opposite, { expr = true })
 
-            -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+            -- -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
             vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
             vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
             vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
